a9ad7237cd4f5ef035f03170f7a31c1f7cc630c5
”s-gwei“ <2277145603@qq.com>
2023-02-26 16:14:04 +0800

    create gtgj demo

---
+++ 12306/src/main/java/com/sun/Application.java
@@ -0,0 +1,15 @@
+package com.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author sungw
+ * @Date 2023/2/26 16:11
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
