d68f0d19b3fd99c90d886582c543be34a08816a9
”s-gwei“ <2277145603@qq.com>
2023-03-04 16:43:30 +0800

    配置接口swagger

--- 12306/src/main/java/com/sun/controller/TestController.java
+++ 12306/src/main/java/com/sun/controller/TestController.java
@@ -2,6 +2,8 @@ package com.sun.controller;
 
 import com.sun.result.Result;
 import com.sun.result.ResultStatus;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author sungw
  * @Date 2023/3/4 15:39
  **/
+
+@Api("测试")
 @RestController
 public class TestController {
 
+    @ApiOperation("获取用户列表")
     @GetMapping("/success")
     public Result test(){
         return Result.success();
